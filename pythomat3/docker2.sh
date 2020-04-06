d6a287e2606d3233e10b7d6fbaad42ecc6c3463a
Marvin Meller <uqvob@student.kit.edu>
2020-04-06 11:13:22 +0200

    Docker 2*

--- pythomat3/docker2.sh
+++ pythomat3/docker2.sh
@@ -2,5 +2,5 @@
 PYTHOMAT=$1
 SOLUTION=$2
 TARGET=$3
-docker run -it -v $SOLUTION:/prod/solution -v $TARGET:/prod/target pythomat:3
+docker run -it -v $PYTHOMAT:/prod/pythomat.zip $SOLUTION:/prod/solution -v $TARGET:/prod/target pythomat:3
 firefox $TARGET/solution.html
